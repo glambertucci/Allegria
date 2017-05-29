1d4e5f78bce0147d0ef2826b367b15d6a89ed067
guido <guido@guido-desktop>
2017-05-29 18:30:08 -0300

    Funciona bien

--- Guess_the_number/main.c
+++ Guess_the_number/main.c
@@ -2,58 +2,49 @@
 #include <time.h>
 #include <stdlib.h>
 
-
-
 #define MAX_NUM 10
 #define MAX_TIME 20
 #define PI 3.14159265359
  
 int main ()
 {
-	 
-	//MANEJO DEL TIEMPO
-	unsigned int x_seconds=0;   
-	unsigned int x_milliseconds=0;
-	unsigned int totaltime=0,time_in_secs=0;
-        int time_left=0;
-        // VARIABLES UTILES
-        int safe_print=10;  //Asegura que no se imprima muchas veces el mensaje del tiempo
-        char start_game;    //el getchar que perimte iniciar el juego
-        //VARIABLES  DE JUEGO
-        int win_number;     
-        char guess;
-        int end_game=1;
+	    //MANEJO DEL TIEMPO
+            unsigned int x_seconds=0;   
+            unsigned int x_milliseconds=0;
+            unsigned int totaltime=0,time_in_secs=0;
+            int time_left=0;
+            // VARIABLES UTILES
+            int safe_print=10;  //Asegura que no se imprima muchas veces el mensaje del tiempo
+            char start_game;    //el getchar que perimte iniciar el juego
+            //VARIABLES  DE JUEGO
+            int win_number;     
+            char guess;
+            int end_game=1;
         
         
-        //MENSAJES
-        char* msg_welcome="Ingrese cualquier caracter para comenzar\nTendra";
-        char *msg_welcome2="segundos para adivinar un numero entre los numeros 0 y";
-        char * msg_welcome3="Una ves ingresado el numero presione enter";
-        char* msg_time_left="Te quedan ";
-        char *msg_wrong="Equivocado, intente nuevamente";
-        char* msg_congrats="Felicitaciones, ha ganado";
-        char* msg_lost="El tiempo termino, usted ha perdido";
+             //MENSAJES
+            char* msg_welcome="Ingrese cualquier caracter para comenzar\nTendra";
+            char *msg_welcome2="segundos para adivinar un numero entre los numeros 0 y";
+            char * msg_welcome3="Una ves ingresado el numero presione enter";
+            char* msg_time_left="Te quedan ";
+            char *msg_wrong="Equivocado, intente nuevamente";
+            char* msg_congrats="Felicitaciones, ha ganado";
+            char* msg_lost="El tiempo termino, usted ha perdido";
         
-        srand (time(NULL)*PI);
+            srand (time(NULL)*PI);
         
-        win_number= rand()%MAX_NUM;
+            win_number= rand()%MAX_NUM;
         
             printf("%s %d %s %d\n%s \n",msg_welcome, MAX_TIME , msg_welcome2, MAX_NUM,msg_welcome3);
-            
             start_game=get_char();
-                          
-            clock_t x_startTime,x_countTime;
-            
+            clock_t x_startTime,x_countTime;     
             time_in_secs=MAX_TIME; 
- 
- 
             x_startTime=clock();  // comienza reloj
             
             time_left=time_in_secs-x_seconds;   // inicio el tiempo restante
 
             while (((time_left>0)) && (guess!=win_number)) 
                 {
-
                         x_countTime=clock();    //actualizo el timer
                         x_milliseconds=x_countTime-x_startTime;
                         x_seconds=(x_milliseconds/(CLOCKS_PER_SEC));
@@ -73,7 +64,6 @@ int main ()
                         guess = getchar(); 
                         guess-='0';
                         }
-                       
                 }
             if(guess==win_number)
             {
@@ -83,9 +73,7 @@ int main ()
             {
                 printf( "\n%s\n",msg_lost);
             }
-           end_game=0;
-                
-      
-        
+            end_game=0;
+                  
 return 0;
 }
