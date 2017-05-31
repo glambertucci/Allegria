cb2c582ca2141f5bb16393c8207c9f32b82ae562
Francisco <ftolaba@itba.edu.ar>
2017-05-31 17:38:27 -0300

    arreglos para el ingreso de unico char con getchar

--- Guess_the_number/main.c
+++ Guess_the_number/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include "get_char.h"
+#include "nonblock.h"
 
-#define MAX_NUM 10
-#define MAX_TIME 20
+
+#define MAX_NUM 9
+#define MAX_TIME 5
 #define PI 3.14159265359
  
 int main ()
@@ -11,32 +14,31 @@ int main ()
 	    //MANEJO DEL TIEMPO
             unsigned int x_seconds=0;   
             unsigned int x_milliseconds=0;
-            unsigned int totaltime=0,time_in_secs=0;
+            unsigned int time_in_secs=0;
             int time_left=0;
             // VARIABLES UTILES
             int safe_print=10;  //Asegura que no se imprima muchas veces el mensaje del tiempo
-            char start_game;    //el getchar que perimte iniciar el juego
+
             //VARIABLES  DE JUEGO
             int win_number;     
-            char guess;
-            int end_game=1;
+            char guess='g';
         
         
              //MENSAJES
             char* msg_welcome="Ingrese cualquier caracter para comenzar\nTendra";
             char *msg_welcome2="segundos para adivinar un numero entre los numeros 0 y";
-            char * msg_welcome3="Una ves ingresado el numero presione enter";
+            char * msg_welcome3="Una vez ingresado el numero presione enter";
             char* msg_time_left="Te quedan ";
-            char *msg_wrong="Equivocado, intente nuevamente";
+    //        char *msg_wrong="Equivocado, intente nuevamente";
             char* msg_congrats="Felicitaciones, ha ganado";
             char* msg_lost="El tiempo termino, usted ha perdido";
         
             srand (time(NULL)*PI);
         
             win_number= rand()%MAX_NUM;
-        
+            printf("winner = %d\n", win_number);
             printf("%s %d %s %d\n%s \n",msg_welcome, MAX_TIME , msg_welcome2, MAX_NUM,msg_welcome3);
-            start_game=get_char();
+            get_char();
             clock_t x_startTime,x_countTime;     
             time_in_secs=MAX_TIME; 
             x_startTime=clock();  // comienza reloj
@@ -56,14 +58,16 @@ int main ()
                             if (!(x_seconds==safe_print))
                             {
                             safe_print=x_seconds;
-                            printf(" \n%s: %d s\n",msg_time_left, time_left);
+                            printf("\n%s: %d s\n",msg_time_left, time_left);
                             }
                         }
+                        nonblock(NB_ENABLE);
                         if (kbhit())
                         {
-                        guess = getchar(); 
-                        guess-='0';
+                            guess = get_char(); 
+                            guess-='0';     // para convertir a numero
                         }
+                        nonblock(NB_DISABLE);
                 }
             if(guess==win_number)
             {
@@ -72,8 +76,6 @@ int main ()
             else
             {
                 printf( "\n%s\n",msg_lost);
-            }
-            end_game=0;
-                  
+            }    
 return 0;
 }
